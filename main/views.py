89a84a2146ec699e2ff49f2587b2ebe3951fb9c5
rawfishthelgh <rg970604@naver.com>
2021-10-05 21:39:58 +0900

    calendar

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
 def showmain(request):
